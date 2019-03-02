b6349276fc1a278003543cab8eeaa5416df4a127
Raihan <raihanislam.cse@gmail.com>
2019-03-02 21:45:01 +0600

    init

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        foreach(range(1,10) as $index){
+            DB::table('users')->insert([
+                'name' => $faker->name,
+                'email' =>  $faker->email,
+                'password' => bcrypt('secret'),
+            ]);
+        }
+        
+    }
+}
